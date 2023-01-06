19cf867d75a3ae0ed3bd645921854756b56b2021
liaozijian <lzj960515@163.com>
2023-01-06 17:21:10 +0800

    算法题

--- solution/src/com/solution/tuling/DpForGame.java
+++ solution/src/com/solution/tuling/DpForGame.java
@@ -6,36 +6,48 @@ package com.solution.tuling;
  */
 public class DpForGame {
 
-    public int dpForGame(int[] value, int[] size, int bagSize){
-        // 三个物品，价值和占格分别为
-        // 1. 1500 | 1
-        // 2. 2000 | 3
-        // 3. 3000 | 4
-        // 背包剩余格子：n
-        // 求能放入背包的最大价值
-        // 设 f(i) 为背包占格子数为i时的最大价值
-        // 当放入一个占格子数为m物品时
-        // f(m) = Math.max(m_value + f(n-m), f(m))
-        int[] dp = new int[bagSize+1];
-        for (int i = 0; i < value.length; i++) {
-            // 遍历背包的格子，计算当格子大小为j时的最大价值
-            for (int j = 1; j <= bagSize; j++) {
+    public static int dp(int[] value, int[] size, int bagSize){
+        // 当为物品i，背包n时的最大价值
+        int[][] dp = new int[value.length+1][bagSize+1];
+        for (int i = 1; i <= value.length; i++) {
+            // 遍历背包的格子，计算当格子大小为n时的最大价值
+            for (int n = 1; n <= bagSize; n++) {
                 // 当前物品所占格子数
-                int curSize = size[i];
-                if(curSize <= j){
+                int m = size[i-1];
+                if(m <= n){
                     // 放得下
-                    // f(j) = Math.max(i_value + f(bagSize-curSize), f(j))
-                    dp[j] = Math.max(value[i] + dp[bagSize-curSize], dp[j]);
+                    // f(i,n) = Math.max(i_value + f(n-m), f(i-1,n))
+                    dp[i][n] = Math.max(value[i-1] + dp[i-1][n-m], dp[i-1][n]);
+                }else {
+                    // f(i,n) = f(i-1,n)
+                    dp[i][n] = dp[i-1][n];
                 }
             }
         }
-        return dp[dp.length-1];
+        return dp[dp.length-1][dp[0].length-1];
     }
 
     public static void main(String[] args) {
-        DpForGame dpForGame = new DpForGame();
-        System.out.println(dpForGame.dpForGame(new int[]{1500, 2000, 3000}, new int[]{1,3,4}, 4));
-        System.out.println(dpForGame.dpForGame(new int[]{2000, 2000, 5000}, new int[]{1,3,4}, 4));
-        System.out.println(dpForGame.dpForGame(new int[]{10, 20, 50}, new int[]{1,2,4}, 5));
+        System.out.println(dp(new int[]{1500, 2000, 3000}, new int[]{1,3,4}, 4));
+        System.out.println(dp(new int[]{1500, 2000, 3000}, new int[]{1,3,4}, 5));
+        System.out.println(dp(new int[]{10, 20, 50}, new int[]{1,2,4}, 5));
+        System.out.println(dp(new int[]{6, 10, 12}, new int[]{1,2,4}, 5));
+    }
+
+    public int balancedStringSplit(String s) {
+        int count = 0;
+        int cur = 0;
+        char[] chars = s.toCharArray();
+        for (char c : chars) {
+            if(c == 'R'){
+                cur++;
+            }else {
+                cur--;
+            }
+            if(cur == 0){
+                count++;
+            }
+        }
+        return count;
     }
 }
